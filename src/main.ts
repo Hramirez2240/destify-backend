47a072974cadc0f08382aee61b06ec4c14fea38d
Hector Ramirez Rodriguez <hector.ramirez@solvex.com.do>
2025-06-13 11:46:18 -0400

    feat: cors config

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,25 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api');
 
-    const config = new DocumentBuilder()
-      .setTitle('User Authentication')
-      .setDescription(
-        'The API details for the User Authentication Demo application using Firebase in the NestJS backend.',
-      )
-      .setVersion('1.0')
-      .addTag('Authentication')
-      .addBearerAuth()
-      .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('User Authentication')
+    .setDescription(
+      'The API details for the User Authentication Demo application using Firebase in the NestJS backend.',
+    )
+    .setVersion('1.0')
+    .addTag('Authentication')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
